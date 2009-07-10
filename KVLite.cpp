78fbb9123d73a09c39f3eec89dd7a0e7a824493c
Jon <jncraton@joncraton.org>
2009-07-10 12:54:04 +0000

    - fixed a bug in HTTP

--- KVLite.cpp
+++ KVLite.cpp
@@ -1,17 +1,13 @@
-/* J. David's webserver */
-/* This is a simple webserver.
- * Created November 1999 by J. David Blackstone.
- * CSE 4344 (Network concepts), Prof. Zeigler
- * University of Texas at Arlington
+/* This is a simple key value store accessible over http.
+ * Based off of code by J. David Blackstone
+ * http://sourceforge.net/projects/tinyhttpd/
  */
-/* This program compiles for Sparc Solaris 2.6.
- * To compile for Linux:
- *  1) Comment out the #include <pthread.h> line.
- *  2) Comment out the line that defines the variable newthread.
- *  3) Comment out the two lines that run pthread_create().
- *  4) Uncomment the line that runs accept_request().
- *  5) Remove -lsocket from the Makefile.
- */
+ 
+/* The syntax is simple
+ * kvlite always accepts GET requests
+ * /get/[key]
+ * /set?[key]=[value]
+
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -29,18 +25,14 @@
 
 #define ISspace(x) isspace((int)(x))
 
-//#define SERVER_STRING "Server: kvlite/0.1.0\r\n"
+#define SERVER_STRING "Server: kvlite/0.1.0\r\n"
 
 void accept_request(int);
 void bad_request(int);
-void cat(int, FILE *);
-void cannot_execute(int);
 void error_die(const char *);
-void execute_cgi(int, const char *, const char *, const char *);
 int get_line(int, char *, int);
-void headers(int, const char *);
+void headers(int);
 void not_found(int);
-void serve_file(int, const char *);
 int startup(u_short *);
 void unimplemented(int);
 
@@ -54,13 +46,12 @@ void accept_request(int client) {
     int numchars;
     char method[255];
     char url[255];
-    char path[512];
     size_t i, j;
-    struct stat st;
     int cgi = 0;      /* becomes true if server decides this is a CGI
                                 * program */
     char *query_string = NULL;
 
+    /* Parse request method */
     numchars = get_line(client, buf, sizeof(buf));
     i = 0; j = 0;
     while (!ISspace(buf[j]) && (i < sizeof(method) - 1)) {
@@ -69,14 +60,7 @@ void accept_request(int client) {
     }
     method[i] = '\0';
 
-    if (strcasecmp(method, "GET") && strcasecmp(method, "POST")) {
-        unimplemented(client);
-        return;
-    }
-
-    if (strcasecmp(method, "POST") == 0)
-        cgi = 1;
-
+    /* Parse request URL */
     i = 0;
     while (ISspace(buf[j]) && (j < sizeof(buf)))
         j++;
@@ -86,6 +70,7 @@ void accept_request(int client) {
     }
     url[i] = '\0';
 
+    /* Remove GET parameters from URL */
     if (strcasecmp(method, "GET") == 0) {
         query_string = url;
         while ((*query_string != '?') && (*query_string != '\0'))
@@ -97,27 +82,15 @@ void accept_request(int client) {
         }
     }
 
-    sprintf(path, "htdocs%s", url);
-    if (path[strlen(path) - 1] == '/')
-        strcat(path, "index.html");
-    if (stat(path, &st) == -1) {
-        while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
-            numchars = get_line(client, buf, sizeof(buf));
-        not_found(client);
-    }
-    else {
-        if ((st.st_mode & S_IFMT) == S_IFDIR)
-            strcat(path, "/index.html");
-        if ((st.st_mode & S_IXUSR) ||
-                  (st.st_mode & S_IXGRP) ||
-                  (st.st_mode & S_IXOTH)    )
-            cgi = 1;
-        if (!cgi)
-            serve_file(client, path);
-        else
-            execute_cgi(client, path, method, query_string);
-    }
+    /* read & discard headers */
+    while ((numchars > 0) && strcmp("\n", buf))  
+        numchars = get_line(client, buf, sizeof(buf));
 
+    headers(client);
+
+    sprintf(buf, "<p>Hello World\r\n\r\n");
+    send(client, buf, strlen(buf), 0);    
+    
     close(client);
 }
 
@@ -141,40 +114,6 @@ void bad_request(int client) {
 }
 
 /**********************************************************************/
-/* Put the entire contents of a file out on a socket.  This function
- * is named after the UNIX "cat" command, because it might have been
- * easier just to do something like pipe, fork, and exec("cat").
- * Parameters: the client socket descriptor
- *             FILE pointer for the file to cat */
-/**********************************************************************/
-void cat(int client, FILE *resource) {
-    char buf[1024];
-
-    fgets(buf, sizeof(buf), resource);
-    while (!feof(resource)) {
-        send(client, buf, strlen(buf), 0);
-        fgets(buf, sizeof(buf), resource);
-    }
-}
-
-/**********************************************************************/
-/* Inform the client that a CGI script could not be executed.
- * Parameter: the client socket descriptor. */
-/**********************************************************************/
-void cannot_execute(int client) {
-    char buf[1024];
-
-    sprintf(buf, "HTTP/1.0 500 Internal Server Error\r\n");
-    send(client, buf, strlen(buf), 0);
-    sprintf(buf, "Content-type: text/html\r\n");
-    send(client, buf, strlen(buf), 0);
-    sprintf(buf, "\r\n");
-    send(client, buf, strlen(buf), 0);
-    sprintf(buf, "<P>Error prohibited CGI execution.\r\n");
-    send(client, buf, strlen(buf), 0);
-}
-
-/**********************************************************************/
 /* Print out an error message with perror() (for system errors; based
  * on value of errno, which indicates system call errors) and exit the
  * program indicating an error. */
@@ -182,96 +121,6 @@ void cannot_execute(int client) {
 void error_die(const char *sc) {
     perror(sc);
     exit(1);
-}
-
-/**********************************************************************/
-/* Execute a CGI script.  Will need to set environment variables as
- * appropriate.
- * Parameters: client socket descriptor
- *             path to the CGI script */
-/**********************************************************************/
-void execute_cgi(int client, const char *path,
-                             const char *method, const char *query_string) {
-    char buf[1024];
-    int cgi_output[2];
-    int cgi_input[2];
-    pid_t pid;
-    int status;
-    int i;
-    char c;
-    int numchars = 1;
-    int content_length = -1;
-
-    buf[0] = 'A'; buf[1] = '\0';
-    if (strcasecmp(method, "GET") == 0)
-        while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
-            numchars = get_line(client, buf, sizeof(buf));
-    else    /* POST */ {
-        numchars = get_line(client, buf, sizeof(buf));
-        while ((numchars > 0) && strcmp("\n", buf)) {
-            buf[15] = '\0';
-            if (strcasecmp(buf, "Content-Length:") == 0)
-                content_length = atoi(&(buf[16]));
-            numchars = get_line(client, buf, sizeof(buf));
-        }
-        if (content_length == -1) {
-            bad_request(client);
-            return;
-        }
-    }
-
-    sprintf(buf, "HTTP/1.0 200 OK\r\n");
-    send(client, buf, strlen(buf), 0);
-
-    if (pipe(cgi_output) < 0) {
-        cannot_execute(client);
-        return;
-    }
-    if (pipe(cgi_input) < 0) {
-        cannot_execute(client);
-        return;
-    }
-
-    if ( (pid = fork()) < 0 ) {
-        cannot_execute(client);
-        return;
-    }
-    if (pid == 0)  /* child: CGI script */ {
-        char meth_env[255];
-        char query_env[255];
-        char length_env[255];
-
-        dup2(cgi_output[1], 1);
-        dup2(cgi_input[0], 0);
-        close(cgi_output[0]);
-        close(cgi_input[1]);
-        sprintf(meth_env, "REQUEST_METHOD=%s", method);
-        putenv(meth_env);
-        if (strcasecmp(method, "GET") == 0) {
-            sprintf(query_env, "QUERY_STRING=%s", query_string);
-            putenv(query_env);
-        }
-        else {   /* POST */
-            sprintf(length_env, "CONTENT_LENGTH=%d", content_length);
-            putenv(length_env);
-        }
-        execl(path, path, NULL);
-        exit(0);
-    } else {    /* parent */
-        close(cgi_output[1]);
-        close(cgi_input[0]);
-        if (strcasecmp(method, "POST") == 0)
-            for (i = 0; i < content_length; i++) {
-                recv(client, &c, 1, 0);
-                write(cgi_input[1], &c, 1);
-            }
-        while (read(cgi_output[0], &c, 1) > 0)
-            send(client, &c, 1, 0);
-
-        close(cgi_output[0]);
-        close(cgi_input[1]);
-        waitpid(pid, &status, 0);
-    }
 }
 
 /**********************************************************************/
@@ -320,9 +169,8 @@ int get_line(int sock, char *buf, int size) {
 /* Parameters: the socket to print the headers on
  *             the name of the file */
 /**********************************************************************/
-void headers(int client, const char *filename) {
+void headers(int client) {
     char buf[1024];
- (void)filename;  /* could use filename to determine file type */
 
     strcpy(buf, "HTTP/1.0 200 OK\r\n");
     send(client, buf, strlen(buf), 0);
@@ -330,6 +178,8 @@ void headers(int client, const char *filename) {
     strcpy(buf, SERVER_STRING);
     send(client, buf, strlen(buf), 0);
     #endif
+    sprintf(buf, "Connection: close\r\n");
+    send(client, buf, strlen(buf), 0);
     sprintf(buf, "Content-Type: text/html\r\n");
     send(client, buf, strlen(buf), 0);
     strcpy(buf, "\r\n");
@@ -362,32 +212,6 @@ void not_found(int client) {
     send(client, buf, strlen(buf), 0);
     sprintf(buf, "</BODY></HTML>\r\n");
     send(client, buf, strlen(buf), 0);
-}
-
-/**********************************************************************/
-/* Send a regular file to the client.  Use headers, and report
- * errors to client if they occur.
- * Parameters: a pointer to a file structure produced from the socket
- *              file descriptor
- *             the name of the file to serve */
-/**********************************************************************/
-void serve_file(int client, const char *filename) {
-    FILE *resource = NULL;
-    int numchars = 1;
-    char buf[1024];
-
-    buf[0] = 'A'; buf[1] = '\0';
-    while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
-        numchars = get_line(client, buf, sizeof(buf));
-
-    resource = fopen(filename, "r");
-    if (resource == NULL)
-        not_found(client);
-    else {
-        headers(client, filename);
-        cat(client, resource);
-    }
-    fclose(resource);
 }
 
 /**********************************************************************/
@@ -458,14 +282,14 @@ int main(void) {
     int client_sock = -1;
     struct sockaddr_in client_name;
     socklen_t client_name_len = sizeof(client_name);
-    char hash[33];
+    /*char hash[33];*/
 
     server_sock = startup(&port);
     printf("kvlite running on port %d\n", port);
     
-    md5("hello",hash);
+    /*md5("hello",hash);
     printf("md5 test %s\n",hash);
-    
+    */
     while (1) {
         client_sock = accept(server_sock,
                                    (struct sockaddr *)&client_name,
